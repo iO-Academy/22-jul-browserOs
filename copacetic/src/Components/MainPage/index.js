91929b73267c9b9ae46cef0e7abe707fd1b67061
josh-lew <joshlewisdev@gmail.com>
2022-10-17 15:27:39 +0100

    commiting missing file

--- copacetic/src/Components/MainPage/index.js
+++ copacetic/src/Components/MainPage/index.js
@@ -1,11 +1,26 @@
+import { useState, useEffect } from "react"
 import NavBar from "../NavBar"
 import './styles.css';
 import cuttlefish from '../../images/cuttlefish-image.png'
+import About from "../About";
 
 function MainPage() {
 
+    const [isDisplayed, setIsDisplayed] = useState(false)
+
+    const handleAboutClick = () => {
+        setIsDisplayed(!isDisplayed)
+    }
+
+
     return (
         <>
+        {isDisplayed &&
+            <About 
+            isDisplayed = {isDisplayed}
+            setIsDisplayed = {setIsDisplayed}/>
+        }
+        <button onClick={handleAboutClick}>About</button>
         <img id="cuttlefishImg" src={cuttlefish} alt="cuttlefish logo" className="mx-auto img-fluid d-block"></img>
         <NavBar />
         </>
