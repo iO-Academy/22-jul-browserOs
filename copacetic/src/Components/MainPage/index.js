92a878bb759c8cdae24e9993818a94e0c5f43fe3
erc-clarkson <erc.clarkson@btinternet.com>
2022-10-17 15:41:40 +0100

    transferred background image to css

--- copacetic/src/Components/MainPage/index.js
+++ copacetic/src/Components/MainPage/index.js
@@ -1,16 +1,15 @@
 import NavBar from "../NavBar"
 import './styles.css';
-import cuttlefish from '../../images/cuttlefish-image.png'
 import Icons from "../Icons";
 
 function MainPage() {
 
     return (
         <>
-        <img id="cuttlefishImg" src={cuttlefish} alt="cuttlefish logo" className="mx-auto bg-image img-fluid d-block"></img>
+        <div id="background">
         <Icons />
-
         <NavBar />
+        </div>
         </>
     )
 }
