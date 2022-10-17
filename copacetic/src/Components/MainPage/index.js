e59c2ee3a129e40631d70a5a94fe69519c1fc310
Jdixon04 <jdixon0437@googlemail.com>
2022-10-17 14:29:56 +0100

    created the icons componenet and styled the icons, need to work on the positioning

--- copacetic/src/Components/MainPage/index.js
+++ copacetic/src/Components/MainPage/index.js
@@ -1,12 +1,15 @@
 import NavBar from "../NavBar"
 import './styles.css';
 import cuttlefish from '../../images/cuttlefish-image.png'
+import Icons from "../Icons";
 
 function MainPage() {
 
     return (
         <>
         <img id="cuttlefishImg" src={cuttlefish} alt="cuttlefish logo" className="mx-auto img-fluid d-block"></img>
+        <Icons />
+
         <NavBar />
         </>
     )
