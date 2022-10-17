b6a01f368521856be3c9819d5e7e8c6dcefd05df
erc-clarkson <erc.clarkson@btinternet.com>
2022-10-17 14:02:04 +0100

    changed the click event functionality to change the full screen icon to a reduce icon

--- copacetic/src/Components/FullScreen/index.js
+++ copacetic/src/Components/FullScreen/index.js
@@ -1,21 +1,35 @@
 import { useState } from "react"
+import './styles.css'
+import reduceImg from '../../images/focus.png'
+import expandImg from '../../images/full-screen.png'
 
 function FullScreen() {
     const [isFullscreen, setIsFullscreen] = useState(false)
+    const [displayIcon, setDisplayIcon] = useState(true)
 
     const fullScreenClick = () => {
         if(isFullscreen) {
             document.exitFullscreen();
             setIsFullscreen(false)
+            setDisplayIcon(true)
         } else {
             document.body.requestFullscreen()
             setIsFullscreen(true)
+            setDisplayIcon(false)
         }
+    }
+
+    const expand = () => {
+        return <img onClick={fullScreenClick} id="expand" src={expandImg}></img>
+    }
+
+    const reduce = () => {
+        return <img onClick={fullScreenClick} id="reduce" src={reduceImg}></img>
     }
 
     return (
         <>
-            <svg onClick={fullScreenClick} xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M21.414 18.586l2.586-2.586v8h-8l2.586-2.586-5.172-5.172 2.828-2.828 5.172 5.172zm-13.656-8l2.828-2.828-5.172-5.172 2.586-2.586h-8v8l2.586-2.586 5.172 5.172zm10.828-8l-2.586-2.586h8v8l-2.586-2.586-5.172 5.172-2.828-2.828 5.172-5.172zm-8 13.656l-2.828-2.828-5.172 5.172-2.586-2.586v8h8l-2.586-2.586 5.172-5.172z"/></svg>
+            { displayIcon ? expand() : reduce() }
         </>
     )
 }
