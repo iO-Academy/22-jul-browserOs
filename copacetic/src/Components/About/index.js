8f41bb221385fc2f186d7006e3e686f5c95e67f6
josh-lew <joshlewisdev@gmail.com>
2022-10-17 12:27:05 +0100

    images added for about section

---
+++ copacetic/src/Components/About/index.js
@@ -0,0 +1,16 @@
+
+
+function About() {
+
+
+    return (
+        <>  
+            <h3>About</h3>
+            <h5>Meet the Team</h5>
+            <p>Emily</p>
+            <img src="../../images/emily.png"></img>
+        </>
+    )
+}
+
+export default About
