10c25955df89a21740b1783473fb7ff289d03fde
Jdixon04 <jdixon0437@googlemail.com>
2022-10-17 16:35:11 +0100

    added the functionality for the menu button to display the icons

--- copacetic/src/Components/MainPage/index.js
+++ copacetic/src/Components/MainPage/index.js
@@ -1,16 +1,23 @@
 import NavBar from "../NavBar"
 import './styles.css';
 import Icons from "../Icons";
+import { useState } from "react";
+
 
 function MainPage() {
 
+    const [showIcons, setShowIcons] = useState(false)
     return (
         <>
         <div id="background">
         </div>
         <div>
+            {showIcons && 
             <Icons />
-            <NavBar />
+            
+            
+            }
+            <NavBar setShowIcons={setShowIcons} showIcons={showIcons}/>
         </div>
         </>
     )
