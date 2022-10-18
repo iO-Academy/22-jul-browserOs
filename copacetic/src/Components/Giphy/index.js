5859564e675524daa8b594ffb1b80c5762c41880
josh-lew <joshlewisdev@gmail.com>
2022-10-18 16:05:26 +0100

    added giphy to box but handleclick not displaying it

---
+++ copacetic/src/Components/Giphy/index.js
@@ -0,0 +1,32 @@
+import Draggable from 'react-draggable';
+import GetAGif from '../GetAGif';
+import './style.css'
+
+function box({isGifDisplayed, setIsGifDisplayed}) {
+
+    const handleGifClick = () => {
+        setIsGifDisplayed(!isGifDisplayed)
+    }
+
+    return (
+        <>
+        <Draggable handle=".handle">
+            <div className="window">
+                    <div className="buttons">
+                        <div className="close">
+                            <a onClick={handleGifClick} className="closebutton"><span><strong>x</strong></span></a>
+                        </div>
+                    </div>
+                <div className="titlebar handle">
+                    Giphy
+                </div>
+                <div className="no-cursor content">
+                    <GetAGif />
+                </div>
+            </div> 
+        </Draggable> 
+    </>
+    )
+}
+
+export default box
--- copacetic/src/Components/MainPage/index.js
+++ copacetic/src/Components/MainPage/index.js
@@ -3,10 +3,11 @@ import NavBar from "../NavBar"
 import './styles.css';
 import About from "../About";
 import Icons from "../Icons";
-
+import Giphy from "../Giphy";
 function MainPage() {
 
     const [isDisplayed, setIsDisplayed] = useState(false)
+    const [isGifDisplayed, setIsGifDisplayed] = useState(false)
     const [showIcons, setShowIcons] = useState(false)
 
     return (
@@ -16,6 +17,11 @@ function MainPage() {
                     <About 
                     isDisplayed = {isDisplayed}
                     setIsDisplayed = {setIsDisplayed}/>
+                }
+                {isGifDisplayed &&
+                    <Giphy 
+                    isGifDisplayed = {isGifDisplayed}
+                    setIsGifDisplayed = {setIsGifDisplayed}/>
                 }
             </div>
             {showIcons && 
