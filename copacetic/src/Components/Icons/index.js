f4d2a9eef88c40c9ba87f72e3f267b0c1456d433
josh-lew <joshlewisdev@gmail.com>
2022-10-20 09:52:14 +0100

    icon styling update for new icons missing styling

--- copacetic/src/Components/Icons/index.js
+++ copacetic/src/Components/Icons/index.js
@@ -38,18 +38,15 @@ function Icons({isDisplayed, setIsDisplayed, isGifDisplayed, setIsGifDisplayed, 
                     <h1 className="text-center mb-0 px-3 font-weight-bold mt-1">T</h1>
                     <p className="text-center">Themes</p>
                 </div>
-
-                {/* <div className={themeIconStyling}> */}
-
-                <div onClick={handleGifClick} className="iconBackground col border rounded m-3 pointer">
+                <div onClick={handleGifClick} className={themeIconStyling}>
                     <h1 className="text-center mb-0 px-3 font-weight-bold mt-1">G</h1>
                     <p className="text-center">Giphy</p>
                 </div>
-                <div onClick={handleMusicClick} className="iconBackground col border rounded m-3 pointer">
+                <div onClick={handleMusicClick} className={themeIconStyling}>
                     <h1 className="text-center mb-0 px-3 font-weight-bold mt-1">L</h1>
                     <p className="text-center">LoFi</p>
                 </div>
-                <div onClick={handleQuotleClick} className="iconBackground col border rounded m-3 pointer">
+                <div onClick={handleQuotleClick} className={themeIconStyling}>
                     <h1 className="text-center mb-0 px-3 font-weight-bold mt-1">Q</h1>
                     <p className="text-center">Quotlefish</p>
                 </div>
