7d4796eef2eac692000e8e2eb5fdd26de51e8460
erc-clarkson <erc.clarkson@btinternet.com>
2022-10-20 10:19:19 +0100

    on mobile view, all icons are now visible

--- copacetic/src/Components/Icons/index.js
+++ copacetic/src/Components/Icons/index.js
@@ -6,7 +6,7 @@ import { useContext } from 'react'
 function Icons({isDisplayed, setIsDisplayed, isGifDisplayed, setIsGifDisplayed, isMusicDisplayed, setIsMusicDisplayed, isQuotleDisplayed, setIsQuotleDisplayed, isThemeDisplayed, setIsThemeDisplayed}) {
         
     const {theme} = useContext(ThemeContext)
-    const themeIconStyling = theme + ' overlay col border rounded m-3 pointer"'
+    const themeIconStyling = theme + ' overlay col border rounded m-3 pointer'
 
     const handleAboutClick = () => {
         setIsDisplayed(!isDisplayed)
@@ -29,7 +29,7 @@ function Icons({isDisplayed, setIsDisplayed, isGifDisplayed, setIsGifDisplayed, 
 
     return (
         <div className="container d-flex p-2 justify-content-center fixed-bottom mb-6">
-            <div className="row justify-content-center flex-nowrap d-flex p-2" >
+            <div className="row justify-content-center flex-wrap d-flex p-2" >
                 <div onClick={handleAboutClick} className={themeIconStyling}>
                     <h1 className="text-center mb-0 px-3 font-weight-bold mt-1">A</h1>
                     <p className="text-center">About</p>
